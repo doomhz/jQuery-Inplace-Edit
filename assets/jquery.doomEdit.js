996688d1731076f6cab4013c7fb652b5de8f1eb6
Glavan Dumitru <dumitru@fetelespun.ro>
2010-12-03 22:03:12 +0100

    Comments

--- assets/jquery.doomEdit.js
+++ assets/jquery.doomEdit.js
@@ -3,11 +3,17 @@
 *
 * @author Dumitru Glavan
 * @version 1.0
-* 
+* @requires jQuery v1.3.2 or later
+*
 * @example $('.dedit-simple').doomEdit({ajaxSubmit:false, afterFormSubmit: function (data, form, el) {el.text(data);}}); - Simple inline edit
 * @example $('.dedit-textarea').doomEdit({ajaxSubmit:false, editField: '<textarea name="myEditTextarea" rows="10" cols="70"></textarea>', afterFormSubmit: function (data, form, el) {el.text(data);}}); - Inline edit with textarea
 * @example $('.dedit-remote').doomEdit({editForm:{method:'post', action:'remote.html', id:'myeditformid'}, afterFormSubmit: function (data, form, el) {el.text($('input', form).val());alert(data);}}); - Inline edit and remote save with ajax
 * @example $('.dedit-remote-json').doomEdit({editForm:{method:'post', action:'remote_json.html', id:'myeditformid'}, afterFormSubmit: function (data, form, el) {data = $.parseJSON(data);el.text(data.message);alert(data.message);}}); - Inline edit and remote save with ajax with JSON response
+*
+* Examples and documentation at: https://github.com/doomhz/jQuery-Inplace-Edit
+* Dual licensed under the MIT and GPL licenses:
+*   http://www.opensource.org/licenses/mit-license.php
+*   http://www.gnu.org/licenses/gpl.html
 *
 */
 (function ($) {
